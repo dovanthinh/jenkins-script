ded926f34f2e13184d7d4af1de7b6a167cf43dc4
thinhdv <thinhdv90@gmail.com>
2019-01-15 14:51:45 +0700

    add jdeploy.py

---
+++ sysadmin/jenkins/jdeploy.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+
+import adm_mdeploy
+import adm_pkginfo
+import argparse
+
+class ADMDeploy:
+    def __init__(self, _srv):
+        self.pkg_input = []	# List of input that need downloading
+        self.pkgs = {}		# "pkgName": "pkgVer"
+        self.srv = _srv
+  
+    def set_pkgs(self, pkg_list):
+        for pkg_path in pkg_list:
+            print pkg_path
+            # Set pkg list for installing
+            pkginfo = adm_pkgInfo.PkgInfo(pkg_path)
+            if "=" in pkg_path:
+                 pkginfo.process()
+                 self.pkgs[pkginfo.pkgname] = pkginfo.pkgver
+            else:
+                 pkginfo.no_process()
+                 self.pkgs[pkginfo.pkgname] = ""
+
+    def deploy(self):
+        d = adm_deploy.JDeploy(self.pkgs, self.srv)
+        d.dry_deploy
+        d.deploy()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='deploy packages to server/group')
+    parser.add_argument("-p", "--packages", nargs="+", help="packages to be installed")
+    parser.add_argument("-m", "--machine", help="which server")
+    args = parser.parse_args()
+
+    adm_deploy = ADMDeploy(args.machine)
+    adm_deploy.set_pkgs(args.packages)
+    print "Deploy:"
+    adm_deploy.deploy()
